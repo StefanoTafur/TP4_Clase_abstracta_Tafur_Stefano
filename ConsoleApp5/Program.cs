42974542a656d68c2323685268d7602e56f4ef0a
William Oviedo <oviedo7898@gmail.com>
2024-05-15 07:49:57 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,70 @@
+﻿// base Abstract class
+abstract class Animal
+{
+    // Abstract method (does not have a body)
+    public abstract void animalSound();
+    // Regular method
+    public void sleep()
+    {
+        Console.WriteLine("Zzz");
+    }
+    public abstract void animaltype();
+}
+
+// Derived class (inherit from Animal)
+class Pig : Animal
+{
+    public override void animalSound()
+    {
+        // The body of animalSound() is provided here
+        Console.WriteLine("The pig says: wee wee");
+    }
+    public override void animaltype()
+    {
+        Console.WriteLine("The pig is mammal");
+    }
+}
+class Dog : Animal 
+{
+    public override void animalSound()
+    {
+        Console.WriteLine("The dog says: wow wow");
+    }
+    public override void animaltype()
+    {
+        Console.WriteLine("The dog is mammal");
+    }
+}
+class Cat : Animal
+{
+    public override void animalSound()
+    {
+        Console.WriteLine("The cat says: miau miau");
+    }
+
+    public override void animaltype()
+    {
+        Console.WriteLine("The cat is a mammal");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pig myPig = new Pig(); // Create a Pig object
+        myPig.animalSound();  // Call the abstract method
+        myPig.animaltype();
+        myPig.sleep();  // Call the regular method
+
+        Dog myDog = new Dog();
+        myDog.animalSound();
+        myDog.animaltype();
+        myDog.sleep();
+
+
+        Cat myCat = new Cat();
+        myCat.animalSound();
+        myCat.animaltype();
+        myCat.sleep();
+    }
+}
